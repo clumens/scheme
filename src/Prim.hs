c41888dfe0f32b8d611431de1e79d0429e08abbf
Chris Lumens <chris@bangmoney.org>
2016-12-05 15:21:38 -0500

    Rearrange the primitive environment a bit.

--- src/Prim.hs
+++ src/Prim.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module Prim where
+module Prim(primEnv,
+            unop)
+ where
 
 import Exceptions(LispException(..))
 import LispVal(Eval(..), IFunc(..), LispVal(..))
@@ -19,33 +21,54 @@ type Prim   = [(T.Text, LispVal)]
 type Unary  = LispVal -> Eval LispVal
 type Binary = LispVal -> LispVal -> Eval LispVal
 
+--
+-- THE PRIMITIVE ENVIRONMENT
+--
+
+-- This environment contains all the scheme functions that must be implemented in Haskell.
+-- They are the most basic operations supported and are what everything else is built upon.
+-- As much as possible, the standard library should be implemented in scheme itself.  This
+-- environment should be the absolute minumum.
+primEnv :: Prim
+primEnv = [ -- Basic math.
+            ("+",       mkF $ binopFold (numOp (+)) (Number 0)),
+            ("-",       mkF $ binop     (numOp (-))),
+            ("*",       mkF $ binopFold (numOp (*)) (Number 1)),
+            ("even?",   mkF $ unop      (numBool even)),
+            ("odd?" ,   mkF $ unop      (numBool odd)),
+            ("pos?" ,   mkF $ unop      (numBool (< 0))),
+            ("neg?" ,   mkF $ unop      (numBool (> 0))),
+
+            -- Booleans.
+            ("<",       mkF $ binop     (numCmp (<))),
+            ("<=",      mkF $ binop     (numCmp (<=))),
+            (">",       mkF $ binop     (numCmp (>))),
+            ("<=",      mkF $ binop     (numCmp (>=))),
+            ("==",      mkF $ binop     (numCmp (==))),
+            ("and",     mkF $ binopFold (eqOp   (&&)) (Bool True)),
+            ("or",      mkF $ binopFold (eqOp   (||)) (Bool False)),
+            ("eq?",     mkF $ binop     eqCmd),
+            ("bl-eq?",  mkF $ binop     (eqOp (==))),
+
+            -- Strings.
+            ("++",      mkF $ binopFold (strOp (<>)) (String "")),
+
+            -- Lists.
+            ("cons",    mkF Prim.cons),
+            ("car",     mkF Prim.car),
+            ("cdr",     mkF Prim.cdr),
+            ("quote",   mkF quote),
+
+            -- IO.
+            ("file?",   mkF $ unop fileExists),
+            ("slurp",   mkF $ unop slurp) ]
+
+--
+-- HELPERS
+--
+
 mkF :: ([LispVal] -> Eval LispVal) -> LispVal
 mkF = Fun . IFunc
-
-primEnv :: Prim
-primEnv = [ ("+"    , mkF $ binopFold (numOp    (+))  (Number 0) ),
-            ("*"    , mkF $ binopFold (numOp    (*))  (Number 1) ),
-            ("++"   , mkF $ binopFold (strOp    (<>)) (String "")),
-            ("-"    , mkF $ binop $    numOp    (-)),
-            ("<"    , mkF $ binop $    numCmp   (<)),
-            ("<="   , mkF $ binop $    numCmp   (<=)),
-            (">"    , mkF $ binop $    numCmp   (>)),
-            (">="   , mkF $ binop $    numCmp   (>=)),
-            ("=="   , mkF $ binop $    numCmp   (==)),
-            ("even?", mkF $ unop $     numBool   even),
-            ("odd?" , mkF $ unop $     numBool   odd),
-            ("pos?" , mkF $ unop $     numBool  (< 0)),
-            ("neg?" , mkF $ unop $     numBool  (> 0)),
-            ("eq?"  , mkF $ binop      eqCmd ),
-            ("bl-eq?",mkF $ binop $    eqOp     (==)),
-            ("and"  , mkF $ binopFold (eqOp     (&&)) (Bool True)),
-            ("or"   , mkF $ binopFold (eqOp     (||)) (Bool False)),
-            ("cons" , mkF  Prim.cons),
-            ("cdr"  , mkF  Prim.cdr),
-            ("car"  , mkF  Prim.car),
-            ("quote", mkF  quote),
-            ("file?" , mkF $ unop  fileExists),
-            ("slurp" , mkF $ unop  slurp) ]
 
 unop :: Unary -> [LispVal] -> Eval LispVal
 unop op [x]    = op x
@@ -54,21 +77,6 @@ unop _ args    = throw $ NumArgs 1 args
 binop :: Binary -> [LispVal] -> Eval LispVal
 binop op [x,y]  = op x y
 binop _  args   = throw $ NumArgs 2 args
-
-fileExists :: LispVal  -> Eval LispVal
-fileExists (Atom atom)  = fileExists $ String atom
-fileExists (String txt) = Bool <$> liftIO (doesFileExist $ T.unpack txt)
-fileExists val          = throw $ TypeMismatch "read expects string, instead got: " val
-
-slurp :: LispVal  -> Eval LispVal
-slurp (String txt) = liftIO $ withFile (T.unpack txt) ReadMode (readTextFile txt)
-slurp val          = throw $ TypeMismatch "read expects string, instead got: " val
-
-readTextFile :: T.Text -> Handle -> IO LispVal
-readTextFile fileName handle =
-    ifM (hIsEOF handle)
-        (TIO.hGetContents handle >>= return . String)
-        (throw $ IOError $ T.concat [" file does not exist: ", fileName])
 
 binopFold :: Binary -> LispVal -> [LispVal] -> Eval LispVal
 binopFold op farg [a, b]      = op a b
@@ -111,6 +119,10 @@ eqCmd (Bool   x) (Bool   y) = return . Bool $ x == y
 eqCmd  Nil        Nil       = return $ Bool True
 eqCmd  _          _         = return $ Bool False
 
+--
+-- LISTS
+--
+
 cons :: [LispVal] -> Eval LispVal
 cons [x,y@(List yList)] = return $ List $ x:yList
 cons [c]                = return $ List [c]
@@ -132,3 +144,22 @@ cdr x             = throw $ ExpectedList "cdr"
 quote :: [LispVal] -> Eval LispVal
 quote [List xs]   = return $ List $ Atom "quote" : xs
 quote [exp]       = return $ List $ Atom "quote" : [exp]
+
+--
+-- IO
+--
+
+fileExists :: LispVal  -> Eval LispVal
+fileExists (Atom atom)  = fileExists $ String atom
+fileExists (String txt) = Bool <$> liftIO (doesFileExist $ T.unpack txt)
+fileExists val          = throw $ TypeMismatch "read expects string, instead got: " val
+
+slurp :: LispVal  -> Eval LispVal
+slurp (String txt) = liftIO $ withFile (T.unpack txt) ReadMode (readTextFile txt)
+slurp val          = throw $ TypeMismatch "read expects string, instead got: " val
+
+readTextFile :: T.Text -> Handle -> IO LispVal
+readTextFile fileName handle =
+    ifM (hIsEOF handle)
+        (TIO.hGetContents handle >>= return . String)
+        (throw $ IOError $ T.concat [" file does not exist: ", fileName])

b5b6fbf2215916983772c8bef00598747f69aaaf
Chris Lumens <chris@bangmoney.org>
2016-12-26 21:58:30 -0500

    Let Main take programs on the command line.
    All command line arguments are checked to see if they're a file that
    exists.  If so, each is executed in its own environment and the results
    printed out.  If any command line arguments are given, the REPL will not
    be run.
    
    This is mainly for purposes of testing, though it could be useful for
    other things in the future.  Also, I might want to write tests in some
    other way.

--- src/Main.hs
+++ src/Main.hs
@@ -1,12 +1,25 @@
 {-# LANGUAGE OverloadedStrings #-}
 
+import           Control.Conditional(whenM)
+import           Control.Monad(void)
+import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
+import           System.Directory(doesFileExist)
+import           System.Environment(getArgs)
 
-import Eval(basicEnv, evalFile)
+import Eval(basicEnv, evalFile, evalText, safeExec)
 import Repl(mainLoop)
 
 main :: IO ()
 main = do
+    args <- getArgs
+
     contents <- TIO.readFile "library.scm"
     env <- evalFile basicEnv contents
-    mainLoop env
+
+    if (length args > 0)
+    then mapM_ (\arg -> whenM (doesFileExist arg) $ do
+                            s <- TIO.readFile arg
+                            void $ safeExec $ evalText env s)
+               args
+    else mainLoop env
